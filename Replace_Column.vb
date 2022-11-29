0d67b308411542cb9ecd2742434a87502858c57a
igorsilva-ce <59261502+igorsilva-ce@users.noreply.github.com>
2022-11-29 09:49:13 -0300

    Update Replace_Column.vb

--- Replace_Column.vb
+++ Replace_Column.vb
@@ -2,13 +2,3 @@
 Function Replace_Column(Column As String, What As String, Replacement As String)
     Columns(Column).Replace What:=What, Replacement:=Replacement
 End Function
-
-' Retorna a extensão do arquivo com base no caminho e nome
-Public Function Excel_File_Extension(FilePath, Filename)
-    Dim extFind As String
-    Dim sFile As String
-
-    sFile = Dir(FilePath & Filename & "*")
-    extFind = Right$(sFile, Len(sFile) - InStrRev(sFile, "."))
-    Excel_File_Extension = FilePath & Filename & "." & extFind
-End Function
